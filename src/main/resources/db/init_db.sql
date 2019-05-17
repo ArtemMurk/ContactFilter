e6f05c2f5f43d17f8748418ed1f28fe665bbf6b9
win1 <muravlevars@gmail.com>
2019-05-17 20:18:17 +0300

    Change init db

--- src/main/resources/db/init_db.sql
+++ src/main/resources/db/init_db.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS contacts;
+DROP SEQUENCE IF EXISTS contacts_id_seq;
+
+CREATE SEQUENCE contacts_id_seq;
 
 CREATE TABLE IF NOT EXISTS contacts
 (
-	id bigint PRIMARY KEY,
+	id bigint PRIMARY KEY DEFAULT nextval('contacts_id_seq') ,
 	name VARCHAR
 )
