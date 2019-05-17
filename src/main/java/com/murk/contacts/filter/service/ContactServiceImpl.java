921dd51c390bd3ea1babcd1a2db97168854af31e
win1 <muravlevars@gmail.com>
2019-05-17 14:48:55 +0300

    Add mapred properties.

--- src/main/java/com/murk/contacts/filter/service/ContactServiceImpl.java
+++ src/main/java/com/murk/contacts/filter/service/ContactServiceImpl.java
@@ -2,10 +2,20 @@ package com.murk.contacts.filter.service;
 
 import com.murk.contacts.filter.model.Contact;
 import com.murk.contacts.filter.model.ContactsResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.Properties;
 
 @Service
 public class ContactServiceImpl implements ContactsService {
+    private int mappers;
+    @Autowired
+    public ContactServiceImpl(@Qualifier("mapred")Properties mapredProps)
+    {
+        mappers = Integer.parseInt(mapredProps.getProperty("mapperSize"));
+    }
 
     @Override
     public ContactsResponse get(String regexp) {
@@ -14,6 +24,7 @@ public class ContactServiceImpl implements ContactsService {
         contactsResponse.setContact(new Contact(1,"name 1"));
         contactsResponse.setContact(new Contact(2,"name 2"));
         contactsResponse.setContact(new Contact(3,"name 3"));
+        contactsResponse.setContact(new Contact(4,"Mapper size = "+mappers));
 
         return contactsResponse;
     }
